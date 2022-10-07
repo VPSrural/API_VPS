0169427974b3e087c45c1abae792dabec976f32a
guisantosalves <workguisantos@gmail.com>
2022-10-07 11:03:46 -0400

    making auth logical

--- controllers/controllerUser.js
+++ controllers/controllerUser.js
@@ -2,6 +2,7 @@ const express = require('express')
 const db = require('../models/connetion') // connection environment
 const userModel = require('../models/user/user')
 const md5 = require('md5')
+const jwt = require('jsonwebtoken')
 
 module.exports.getUser = async (req, res) => {
     res.setHeader("Content-Type", "application/json")
@@ -24,7 +25,7 @@ module.exports.getUser = async (req, res) => {
     }
 }
 
-module.exports.creatingUser = (req, res) => {
+module.exports.creatingUser = async (req, res) => {
     res.setHeader("Content-type", "application/json")
     if(db()){
 
@@ -33,10 +34,38 @@ module.exports.creatingUser = (req, res) => {
             const login = req.body.login
             const password = md5(req.body.password)
 
+            // force error if the require fields weren't setted
+            if(!(login && password && idAlpha)){
+                res.status(400).send("Login, password e idAlpha sao requeridos")
+            }
+
+            const verifyUser = await userModel.findOne({login})
+            
+            // verify user
+            if(verifyUser){
+                res.status(409).send({error: "usuario ja existe"})
+            }
+            
+            const userToInsert = await userModel.create({
+                idAlphaExpress: idAlpha,
+                login: login,
+                password: password,
+            })
+
+            // creating token
+            const token = jwt.sign({
+                userid: userToInsert._id, 
+                userlogin: userToInsert.login
+            },
+            process.env.TOKEN_KEY)
+
+            // inserting token to user save
+            // the token doesn't go to the DB
+            userToInsert.token = token
 
             console.log(idAlpha, login, password)
-            
-            res.send({"teste":"eaea"})
+
+            res.status(200).send(userToInsert)
         }catch(err){
 
             res.status(400).json({error: err})
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/controllerUser")
 
 //setting the user router
 router.get('/user', userController.creatingUser)
+router.post('/user', userController.creatingUser)
 
 module.exports = router
