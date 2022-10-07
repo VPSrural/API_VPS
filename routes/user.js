4157756708a61e7eb52b2c0f9378807de7d5d3c9
guisantosalves <workguisantos@gmail.com>
2022-10-07 14:46:53 -0400

    the user route is complete

--- routes/user.js
+++ routes/user.js
@@ -5,17 +5,16 @@ const auth = require("../middlewares/auth.js")
 
 // using jwt authentication in this route
 // we send a token to user and save in asyncStorage
-// when 
 
 // o usuario vai fazer o cadastro, logo depois será mostrado na tela de solicitação
 // mudando ativo para true, o usuario tera acesso ao app, se nao, nao
 
 //setting the user router
-router.get('/user', auth, userController.getUser)
-router.get('/userAdm', userController.getUser)
-router.post('/user', userController.creatingUser)
-router.put('/user/:id', userController.updatingUser)
-router.delete('/user/:id', userController.deleteUser)
+router.get('/user', auth, userController.getUser) //user wil make this request to access the ap
+router.get('/userAdm', userController.getUser) // adm request
+router.post('/user', userController.creatingUser) // user registration
+router.put('/user/:id', userController.updatingUser) // user update
+router.delete('/user/:id', userController.deleteUser) // user delete
 // router.put('/user', )
 
 module.exports = router
